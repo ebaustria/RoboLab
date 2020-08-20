ab0e1c337021a59b41e2ec4c488aafe8a7115ea7
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2020-08-20 14:24:23 +0200

    minor changes to init function of robot.py

--- src/robot.py
+++ src/robot.py
@@ -1,11 +1,13 @@
 from planet import Planet
 from communication import Communication
 from odometry import Odometry
+from barrier import Ultrasound
 
 
 class Robot:
 
-    def __init__(self, planet, communication, odometry):
+    def __init__(self, planet: Planet, communication: Communication, odometry: Odometry, ultrasound: Ultrasound):
         self.planet = planet
         self.communication = communication
         self.odometry = odometry
+        self.ultrasound = ultrasound
