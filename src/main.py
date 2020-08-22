d93b89f1db49d2279e4b3462e97c5d8992f46d74
Nico vom Hofe <Nico.vom_Hofe@mailbox.tu-dresden.de>
2020-08-22 13:41:50 +0200

    status 22.08

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def run():
     # Your script isn't able to close the client after crashing.
     global client
 
-    client_id = 'brick-YOURGROUPID'  # Replace YOURGROUPID with your group ID
+    client_id = 'brick-117'  # Replace YOURGROUPID with your group ID
     client = mqtt.Client(client_id=client_id,  # Unique Client-ID to recognize our program
                          clean_session=True,  # We want a clean session after disconnect or abort/crash
                          protocol=mqtt.MQTTv311  # Define MQTT protocol version
@@ -41,88 +41,95 @@ def run():
 
 
     #my changes start
-    '''cs = ev3.ColorSensor()
-    cs.mode = 'RGB-RAW'
-    r, g, b = cs.bin_data("hhh")
-    brightness = math.sqrt(r ** 2 + g ** 2 + b ** 2)
-    # brightness = (r + g + b) / 3
-    # print("Brightness: " + str(brightness))
-    print("red: " + str(r) + " green: " + str(g) + " blue: " + str(b))'''
+
 
     myMotor = Motors()
     myUltrasonic = Ultrasonic()
     myColorSensor = ColorSensor()
-    print(myColorSensor.get_node())
-    #r, g, b = myColorSensor.get_colors()
-    #print("red: " + str(r) + " green: " + str(g) + " blue: " + str(b))
-    count = 0 #better solution?
-    while myUltrasonic.get_distance() > 25:
-        #print(myColorSensor.get_node())
-        if myColorSensor.get_node() == "red" and counter == 0:
-            counter += 1
-            myMotor.stop()
-            myMotor.drive_forward(50, 2)
-            myMotor.stop()
-            #ev3.Sound.speak("Red")
-            myMotor.turn_left(20, 6)
-            time.sleep(2)
-
-        elif myColorSensor.get_node() == "blue" and counter == 0:
-            counter += 1
-            myMotor.stop()
-            myMotor.drive_forward(50, 2)
-            myMotor.stop()
-            #ev3.Sound.speak("Blue")
-            myMotor.turn_left(20, 6)
-            time.sleep(2)
-
-        else:
-            myMotor.follow_line(200,1)
-            counter = 0
-
-    myMotor.drive_backward(200,5)
-
-    
-    #def dif(color, ref_color1, ref_color2): #eventuell lieber Spektren zur Ermittlung von Knoten aufnehmen
-    '''    dif_color = 0
-        if (color - ref_color1 < 0):
-            dif_color += ref_color1 - color
-        else:
-            dif_color += color - ref_color1
-        if (color - ref_color2< 0):
-            dif_color += ref_color2 - color
-        else:
-            dif_color += color - ref_color2
-        return dif_color
-
-    cs = ev3.ColorSensor()
-    cs.mode = 'RGB-RAW'
-    r, g, b = cs.bin_data("hhh")
-    print("red: " + str(r) + " green: " + str(g) + " blue: " + str(b))
-    dif_red = 0
-    dif_blue = dif(b, r , r)
-    print(str(dif_blue))
-
-    while dif_red < 200 :
-        myMotor.follow_line(200,1)
-        r, g, b = cs.bin_data("hhh")
-        print("red: " + str(r) + " green: " + str(g) + " blue: " + str(b))
-        dif_red = dif(r,g,b)
-        print(str(dif_red))
-    while dif_blue < 100:
-        myMotor.follow_line(200, 1)
-        r, g, b = cs.bin_data("hhh")
-        print("red: " + str(r) + " green: " + str(g) + " blue: " + str(b))
-        dif_blue = dif(b, r, r)
-        print(str(dif_blue))
-    myMotor.turn_left(200,5)'''
- #red = 168, 178
- #blue
+    print("Press Button to start")
+    myColorSensor.button_pressed()
 
 
 
+    rm = ev3.LargeMotor("outB")
+    lm = ev3.LargeMotor("outC")
+    myOdometry = Odometry(lm, rm)
+    myOdometry.reset_position()
+    pos_l, pos_r = myOdometry.get_position()
+    print("Position 1 rechts: " + str(pos_r))
+    print("Position 1 links: " + str(pos_l))
+    myMotor.follow_line(1, myColorSensor, myOdometry)
+    #myMotor.drive_forward(100,2)
+    #myMotor.turn_angle(100, 10, 5)
+    #myMotor.drive_forward(100,2) #problem because alpha in that case = 0 -> division zero
+    for tupel in myOdometry.get_tupel_list():
+        a = tupel[0]
+        b = tupel[1]
+        print("A: " + str(a) + "    B: " + str(b))
+    '''pos_l, pos_r = myOdometry.get_position()
+    print("Position 1 rechts: " + str(pos_r))
+    print("Position 1 links: " + str(pos_l))
+    dl, dr = myOdometry.get_distance(pos_l,pos_r)
+    print("Distanz rechts: " + str(dr))
+    print("Distanz links; " + str(dl))
+    alpha = myOdometry.get_angle_alpha(dl,dr)
+    print("Winkel: " + str(alpha*360/(2*math.pi)))
+    length = myOdometry.get_path_length(dl, dr, alpha)
+    print("Länge: " + str(length))'''
 
-        
+
+    '''
+
+    counter = 0 #better solution? -> first drive away from node than scan again
+    while True:
+        while myUltrasonic.get_distance() > 20:
+            if myColorSensor.get_node() == "blue" and counter == 0:
+                # ev3.Sound.speak("Blue")
+                counter += 1
+                myMotor.stop()
+                myMotor.drive_forward(50, 3)
+                myMotor.stop()
+
+                angles = myColorSensor.get_neighbour_nodes()
+
+                i = 0
+                for angle in angles:
+
+                    i += 1
+                    print(str(i) + ". Knoten am Winkel: " + str(angle))
+                    if angle != 10: #only test -> decision for angle later
+                        print("Winkel ungleich 10")
+                        myMotor.turn_angle(100, angle, 4)
+                        time.sleep(1) #necessary?
+                        break
+                time.sleep(2) #necessary?
+
+            elif myColorSensor.get_node() == "red" and counter == 0:
+                #ev3.Sound.speak("Red")
+                counter += 1
+                myMotor.stop()
+                myMotor.drive_forward(50, 3)
+                myMotor.stop()
+
+                angles = myColorSensor.get_neighbour_nodes()
+
+                i = 0
+                for angle in angles:
+                    i += 1
+                    print(str(i) + ". Knoten am Winkel: " + str(angle))
+                    if angle != 10:
+                        print("Winkel ungleich 10")
+                        myMotor.turn_angle(100,angle,4)
+                        time.sleep(1) #necessary?
+                        break
+                time.sleep(2)#necessary?
+
+            else:
+                myMotor.follow_line(0.5, myColorSensor)
+                counter = 0
+        myMotor.turn_angle(100,170,4)
+
+    '''
     #my changes end
 
 
--- src/odometry.py
+++ src/odometry.py
@@ -1,11 +1,55 @@
 # !/usr/bin/env python3
-
+import math
 
 class Odometry:
-    def __init__(self):
-        """
-        Initializes odometry module
-        """
+    def __init__(self, motor_left, motor_right):
+        self.motor_left = motor_left
+        self.motor_right = motor_right
+        self.tick_list = []
 
         # YOUR CODE FOLLOWS (remove pass, please!)
         pass
+
+    def reset_position(self):
+        self.motor_left.command = "reset"
+        self.motor_right.command = "reset"
+
+    def get_position(self):
+        return self.motor_left.position, self.motor_right.position
+
+    def get_distance(self, ticks_l, tick_r):
+        diameter_wheel = 5.5
+        distance_l = diameter_wheel*math.pi*ticks_l/360
+        distance_r = diameter_wheel*math.pi*tick_r/360
+        return distance_l, distance_r
+
+    def get_angle_alpha(self, dl, dr):
+        distance_wheels = 12
+        return (dr-dl)/distance_wheels
+
+    def get_path_length(self, dl, dr, alpha):#alpha in arc measure
+        return (dr + dl)/alpha*math.sin(alpha/2)
+
+    def get_dif_x(self, gamma_old, beta, length):#gamma_old in arc measure
+        return -math.sin(gamma_old + beta) * length
+
+    def get_dif_y(self, gamma_old, beta, length):#gamma_old in arc measure
+        return math.cos(gamma_old + beta) * length
+
+    def get_gamma_new(self, gamma_old, alpha):#gamma_old, alpha in arc measure
+        return gamma_old + alpha
+
+    def add_point(self, tupel_ticks):
+        self.tick_list.append(tupel_ticks)
+
+    def get_tupel_list(self):
+        return self.tick_list
+
+    def add_movement(self, angle_left, angle_right):
+        pass
+
+
+
+    def get_direction(self):
+        pass
+
