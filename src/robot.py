2c00894cbfad6e11ee7c92a3ff8c4d1f8331246a
Eric Bush <eric.bush@mailbox.tu-dresden.de>
2020-08-22 13:42:34 +0200

    Deleted unnecessary barrier file and removedimport from robot.py

--- src/robot.py
+++ src/robot.py
@@ -1,13 +1,11 @@
 from planet import Planet
 from communication import Communication
 from odometry import Odometry
-from barrier import Ultrasound
 
 
 class Robot:
 
-    def __init__(self, planet: Planet, communication: Communication, odometry: Odometry, ultrasound: Ultrasound):
+    def __init__(self, planet: Planet, communication: Communication, odometry: Odometry):
         self.planet = planet
         self.communication = communication
         self.odometry = odometry
-        self.ultrasound = ultrasound
