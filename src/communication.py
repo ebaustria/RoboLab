4c9098ff51a7647da24f06061966678baa2893f2
Nico vom Hofe <Nico.vom_Hofe@mailbox.tu-dresden.de>
2020-08-24 23:26:58 +0200

    status 24.08 test 2

--- src/communication.py
+++ src/communication.py
@@ -339,7 +339,7 @@ class Communication:
 
         self.send_message("planet/%s/117" % self.robot.planet_name, payload)
         
-    def send_path_select(self, choice: Tuple[Tuple[int, int], Direction]):
+    def send_path_select(self, choice: object) -> object:
         """
         Sends to the mothership the pathSelect-Message 
         with the information about which path roboter will take
